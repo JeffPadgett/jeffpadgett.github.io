70324b479320078bf830bedbd511d95aee732ad0
Jeffery Padgett <JeffreyPadgett1@gmail.com>
2019-04-08 15:02:01 -0400

    data structure

--- src/Budget_App/app.js
+++ src/Budget_App/app.js
@@ -1,7 +1,32 @@
 
 
-
+// Budget Controller Keeps track of all the incomes and expenses, budget, and percentages.
 var budgetController = (function () {
+
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+
+    }
+
 
 
 })();
