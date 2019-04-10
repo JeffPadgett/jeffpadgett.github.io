4d9f94e78c2badef8520623992c2a269b3c7a826
JeffPadgett <JeffreyPadgett1@gmail.com>
2019-04-09 23:37:39 -0400

    clear input values

--- src/Budget_App/app.js
+++ src/Budget_App/app.js
@@ -90,7 +90,7 @@ var UIController = (function() {
         },
 
         addListItem: function(obj, type){
-            var html, newHtml, element
+            var html, newHtml, element,
             // Create HTML string with placeholder text
 
             if (type === 'inc'){
@@ -109,6 +109,20 @@ var UIController = (function() {
             //Insert the HTML into the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
 
+
+        },
+
+        clearFields: function () {
+            var fields, fieldsArray;
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ',' + DOMstrings.inputValue);
+
+            fieldsArray = Array.prototype.slice.call(fields);
+
+            fieldsArray.forEach(function (current, index, array) {
+                current.value = "";
+            })
+
+            fieldsArray[0].focus();
 
         },
 
@@ -146,10 +160,14 @@ var controller = (function (budgetCtrl, UICtrl) {
         newItem = budgetController.addItem(input.type, input.descrition, input.value);
 
         // 3. Add the item to the UI
+        UICtrl.addListItem(newItem, input.type);
 
-        // 4. Calculate the budget
+        // 4. Clear the fields
+        UICtrl.clearFields();
 
-        // 5. Display the budget on the UI
+        // 5. Calculate the budget
+
+        // 6. Display the budget on the UI
     };
 
     return {
