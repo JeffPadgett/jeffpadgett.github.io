16564311e84cc271780e73a59ecf7882d399edb6
Jeffery Padgett <JeffreyPadgett1@gmail.com>
2019-04-12 13:59:49 -0400

    item delete functionality

--- src/Budget_App/app.js
+++ src/Budget_App/app.js
@@ -67,6 +67,20 @@ var budgetController = (function () {
             return newItem;
         },
 
+        deleteItem: function(type, id) {
+            var ids, index;
+
+            var ids = data.allItems[type].map(function(current) {
+                return current.id;
+            });
+
+            index = ids.indexOf(id);
+
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
+        },
+
         calculateBudget: function() {
             // calculate total income and expenses
             calculateTotal('exp');
@@ -150,9 +164,11 @@ var UIController = (function() {
 
             //Insert the HTML into the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
-
-
         },
+
+        deleteListItem: function (selectorID) {
+
+        }
 
         clearFields: function () {
             var fields, fieldsArray;
@@ -252,10 +268,10 @@ var controller = (function (budgetCtrl, UICtrl) {
             //inc-1
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1];
+            ID = parseInt(splitID[1]);
 
             // 1. delete the item from the data structure
-
+            budgetCtrl.deleteItem(type, ID);
             // 2. Delete the item from the UI
 
             // 3. Update and show the new budget
