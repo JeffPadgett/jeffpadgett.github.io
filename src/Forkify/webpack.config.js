fb7eb4d9043a35f116ff4aa3236ba24451f1208b
JeffPadgett <JeffreyPadgett1@gmail.com>
2019-05-21 23:25:52 -0400

    babel

--- src/Forkify/webpack.config.js
+++ src/Forkify/webpack.config.js
@@ -15,5 +15,16 @@ module.exports = {
             filename: 'index.html',
             template: './src/index.html'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
 };
