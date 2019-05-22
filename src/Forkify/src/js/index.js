fd5fa4e6f4ebddea3365e43e909625eeecb3b5a2
JeffPadgett <JeffreyPadgett1@gmail.com>
2019-05-21 23:46:59 -0400

    babel complete

--- src/Forkify/src/js/index.js
+++ src/Forkify/src/js/index.js
@@ -1,5 +1,5 @@
 // Global app controller
 import num from './test';
-
-console.log(`I imported ${num} from another module!!`);
+const x = 23;
+console.log(`I imported ${num} from another module!! Var x is ${x}`);
 
