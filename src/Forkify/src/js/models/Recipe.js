5801d211bce5e4482d5d24eebc4cf3e8fe3da5a6
JeffPadgett <JeffreyPadgett1@gmail.com>
2019-06-03 14:02:46 -0400

    Recipe ingredient adder

--- src/Forkify/src/js/models/Recipe.js
+++ src/Forkify/src/js/models/Recipe.js
@@ -93,4 +93,19 @@ export default class Recipe {
         });
         this.ingredients = newIngredients;
     }
+
+    updateServings (type) {
+        //Servings
+        const newServings = type === 'dec' ? this.servings -1 : this.servings +1;
+
+        //Ingredients
+        this.ingredients.forEach(ing => {
+            ing.count *= (newServings / this.servings);
+        });
+
+        this.servings = newServings;
+
+
+    }
+
  }
